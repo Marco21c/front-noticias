b97717cb9df8adeef2778103ed0b0a3871f7d7b0
Marcos21c <marcosecondori@gmail.com>
2026-01-15 23:36:11 -0300

    add: se agrego rutas en app y configuraciones en main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button } from "@/components/ui/button"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DefaultLayout from "./pages/components/DefaultLayout";
+import NotFound from "./pages/components/NotFound";
+import Home from "./pages/Home";
+
+export const router = createBrowserRouter(
+[
+  { path: "/",
+    element: <DefaultLayout/> ,
+    errorElement: <NotFound/> ,
+    children: [
+      { index: true,
+        element: <Home/>
+       },
+    ]
+   }
+]
+);
  
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Listo crack</Button>
-    </div>
-  )
-}
+    <RouterProvider router={router}/>
+  );
+};
  
-export default App
+export default App;
