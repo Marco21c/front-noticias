db8f8cd5211c317d06f6800be5d33da09e5fe47f
Marcos21c <marcosecondori@gmail.com>
2026-01-20 00:41:43 -0300

    fix: fix axios.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,9 +2,9 @@ import axios from "axios"
 
 
 export const apiClient = axios.create({
-    baseURL: import.meta.env.DEV
-    ? import.meta.env.VITE_API_URL_DEV
-    : import.meta.env.VITE_API_URL_PROD,
+    baseURL: import.meta.env.VITE_DEV
+    ? import.meta.env.VITE_BACKEND_URL
+    : import.meta.env.VITE_URL_HOST,
     headers: {
         'Content-Type': 'application/json',
     },
