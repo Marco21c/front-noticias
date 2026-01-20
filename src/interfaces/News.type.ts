ee241ed9b2fc17547159713253fecae69517bb74
Ezequiel <ezequielpacheco.dev@gmail.com>
2026-01-20 16:52:46 -0300

    fix: delete files innecesary

--- src/interfaces/News.type.ts
+++ src/interfaces/News.type.ts
@@ -14,20 +14,4 @@ export interface INews {
   publicationDate: string;
 }
 
-/* export type StatusNews = 'draft' | 'published' | 'archived' ;
-
-export interface NewsApi{
-    _id: string
-    title: string
-    slug: string
-    summary: string
-    content: string
-    highlights: boolean
-    author: string
-    category: string
-    mainImage: string
-    status: string
-    publicationDate: Date
-} */
-
     
--- src/lib/newsVariant.ts
+++
@@ -1,9 +0,0 @@
-import type { News } from '../interfaces/news';
-
-export type NewsVariant = 'highlighted' | 'featured' | 'default';
-
-export const getNewsVariant = (news: News): NewsVariant => {
-    if (news.highlighted) return 'highlighted';
-    if (news.featured) return 'featured';
-    return 'default';
-};
