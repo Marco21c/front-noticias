5b7bde210808da989f1911ca02d84c12d04a0536
Ezequiel <ezequielpacheco.dev@gmail.com>
2026-01-21 18:51:12 -0300

    feat: login page with form validation.

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,35 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultLayout from "./pages/components/DefaultLayout";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export const router = createBrowserRouter(
-[
-  { path: "/",
-    element: <DefaultLayout/> ,
-    children: [
-      { index: true,
-        element: <Home/>
-       },
-       { path: "*",
-         element: <NotFound/>
-       }
-    ]
-   }
-]
+  [
+    {
+      path: "/",
+      element: <DefaultLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "login",
+          element: <Login />
+        },
+        {
+          path: "*",
+          element: <NotFound />
+        }
+      ]
+    }
+  ]
 );
- 
+
 function App() {
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
 };
- 
+
 export default App;
