7df785eb8390fe48b23688121cb2f7ece7286c2c
Ezequiel <ezequielpacheco.dev@gmail.com>
2026-01-21 15:30:38 -0300

    fix: update Home.tsx titles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,18 +7,15 @@ export default function Home() {
   return (
     <>
       <main className="max-w-6xl mx-auto px-4">
-        <div>
-          <h1 className="text-3xl font-serif font-bold mt-8 mb-6 justify-center flex">
-            Últimas Noticias
-          </h1>
-        </div>
-        <h2 className="text-2xl font-serif font-bold mt-8 mb-6 border-b-4 border-gray-600">Destacadas</h2>
+
+        <h1 className="text-2xl font-serif font-bold mt-10 mb-2 border-b-4 border-gray-600">Destacadas</h1>
         <NewsList />
 
-        <h2 className="text-2xl font-serif font-bold mt-8 mb-6 border-b-4 border-gray-600">Importantes</h2>
+        <h2 className="text-2xl font-serif font-bold mt-10 mb-2 border-b-4 border-gray-600">Importantes</h2>
         <NewsFeatured />
 
-        <h2 className="text-2xl font-serif font-bold mt-8 mb-6 border-b-4 border-gray-600">Otras Noticias</h2>
+
+        <h2 className="text-2xl font-serif font-bold mt-10 mb-2 border-b-4 border-gray-600">Otras Noticias</h2>
         <OthersNews />
         <p className="text-gray-600">
           Ultimas noticias de politica, economía, deportes y más.
